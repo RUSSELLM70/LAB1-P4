8b51168020f0ed8e47029fd1be5de18a49a94d97
Russell <russellm434@gmial.com>
2022-06-02 12:18:48 -0600

    First commit -- Hello World xD

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>LAB1-P4!</Text>
+      <Text>LAB1-P4 XD!</Text>
       <StatusBar style="auto" />
     </View>
   );
