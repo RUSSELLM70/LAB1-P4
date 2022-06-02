1866f17b04016bc93ac816902604535172be34b7
Russell <russellm434@gmial.com>
2022-06-02 13:22:41 -0600

    Second commint -- Test labels and styles Tasks

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { KeyboardAvoidingView, StyleSheet, Text, View, TextInput, TouchableOpacity, Keyboard, ScrollView } from 'react-native';
+import Task from './Components/Task';
 
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>LAB1-P4 XD!</Text>
-      <StatusBar style="auto" />
+    
+          {/* Today's Tasks  - P4 Tasks*/}
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>LAB1-P4  Tareas</Text>
+
+        <View style={styles.items}>
+          {/* This is where the tasks will go! */}
+
+          <Task Text={'Task 1'} />
+          <Task Text={'Task 2'} />
+          <Task Text={'Task 3'} />
+
+        </View>
+      </View>
+            
     </View>
   );
 }
@@ -13,8 +28,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED',
+  },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  items: {
+    marginTop: 30,
   },
 });
